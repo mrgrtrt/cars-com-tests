a9b8be4721e902bbf577c0d8953b1021eb7c573e
Маргарита Иванова <mivanova@samokat.ru>
2023-05-22 14:32:08 +0400

    gihub actions

--- src/test/java/com/cars/BaseTest.java
+++ src/test/java/com/cars/BaseTest.java
@@ -5,6 +5,8 @@ import com.microsoft.playwright.Page;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.util.Objects;
+
 import static com.cars.utils.ConfigUtils.ConfigKeys.TIMEOUT;
 import static com.cars.utils.ConfigUtils.ConfigKeys.URL;
 import static com.cars.utils.ConfigUtils.DEFAULTS;
@@ -19,11 +21,11 @@ public class BaseTest {
     public void initBrowser() {
         browser = new PlaywrightContext().getChromeBrowser();
         page = browser.newPage();
-
-        assertTrue(browser.isConnected());
-
         page.setDefaultTimeout(Double.parseDouble(DEFAULTS.get(TIMEOUT)));
         page.navigate(DEFAULTS.get(URL));
+
+
+        assertTrue(Objects.nonNull(page));
     }
 
     @AfterSuite
